c90e6637565756227f1da6cbf94f0ccf8612243b
NED-dev-2020 <exaucengango2@gmail.com>
2022-09-10 06:31:36 +0100

    commit de sango-events

---
+++ index.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/fontawesome-free-5.15.4-web/css/all.css">
+    <link rel="stylesheet" href="assets/bootstrap-5.2.0-dist/bootstrap-5.2.0-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="header container-fluid">
+        <div class="degrade">
+            <div class="container">
+                <h1>Sango-Events</h1>
+                <p>Spécialisé dans la promotion des événements avenir au congo</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5 pb-5">
+    
+        <!--Evenement categorie-->
+        <div class="row">
+            <div class="col-12">
+                <h2 class="categorie px-2">Evenements Religieux</h2>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/image5.jpg');">
+                    <div class="degrade">
+                        <h2>Marathon de prière</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>23, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Brazzaville</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Evenement block-->
+
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/image2.jpg');">
+                    <div class="degrade">
+                        <h2>Seminaire d'évangelisation</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>23, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Brazzaville</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Evenement block-->
+
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/image3.jpg');">
+                    <div class="degrade">
+                        <h2>Seminaire de prière</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>23, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Brazzaville</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Evenement block-->
+
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/image_1.jpg');">
+                    <div class="degrade">
+                        <h2>Retraite spirituelle</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>23, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>I.C.C de brazza</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Evenement block-->
+
+        </div>
+        <!--Evenement categorie-->
+
+
+
+        <!--Evenement categorie-->
+        <div class="row">
+            <div class="col-12">
+                <h2 class="categorie px-2">Evenements culturels</h2>
+            </div>
+        </div>
+
+        <div class="row">
+
+             <!--Evenement block-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/275632559_515766943249402_6566352475522616980_n.jpg');">
+                    <div class="degrade">
+                        <h2>Ceremonie De Remise De Certificats</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>10, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Grande Bibliotheque universitaire </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+             <!--Evenement block-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/culture1.jpg');">
+                    <div class="degrade">
+                        <h2>Journée internatonnale<br> de la jeunesse</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>12,aout 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>n'kayi</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!--Evenement block-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/culture3.jpg');">
+                    <div class="degrade">
+                        <h2>Brazza Fashion</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>27,Mars 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Brazzaville</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+             <!--Evenement block-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/289556104_583694803123282_3499675804663677917_n.jpg">
+                    <div class="degrade">
+                        <h2>Ceremonie De Remise De Certificats</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>10, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Grande Bibliotheque universitaire</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+             
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/culture2.jpg');">
+                    <div class="degrade">
+                        <h2>colonie de vaccance</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>Du 10 au 29, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span> pointe-noire</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Evenement block-->
+
+             <!--Evenement block-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/remise.jpg">
+                    <div class="degrade">
+                        <h2>Ceremonie De Remise De Certificats</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>10, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Grande Bibliotheque universitaire</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Evenement block-->
+
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/culture4.jpg');">
+                    <div class="degrade">
+                        <h2>foire culturelle</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>Du 03 au 12, mars 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>memorial DE BRAZZA</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Evenement block-->
+
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/culture1.jpg');">
+                    <div class="degrade">
+                        <h2>Journée internatonnale<br> de la jeunesse</h2>
+                        <div class="">
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>12,aout 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>n'kayi</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+             <!--Evenement block--> 
+             
+                <!--Evenement categorie-->
+         <div class="row">
+            <div class="col-12">
+                <h2 class="categorie px-2">Evenements sportifs</h2>
+            </div>
+        <div class="row">
+            <!--Evenement block-->
+            <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/sport12.jpg');">
+                    <div class="degrade">
+                        <h2>CONGO vs R.C.A</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>04, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Brazzaville</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!--Evenement block-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/IMG-20210925-WA0020.jpg');">
+                    <div class="degrade">
+                        <h2>MATCH DE GALLA FEMININ</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>23, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Brazzaville</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!--Evenement b-->lock-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/sport4.jpg');">
+                    <div class="degrade">
+                        <h2>Semi Marathon internatonnal de Brazzaville
+                             SMIB
+                        </h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>23, sept 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Brazzaville</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!--Evenement block-->
+             <div class="col-md-3 p-1">
+                <div class="block-event" style="background-image: url('assets/images/Congo_Republic_FA.png');">
+                    <div class="degrade">
+                        <h2 >Election a la Presidence<br> de la FECOFOOT</h2>
+                        <div>
+                            <i class="fa fa-calendar-alt"></i>
+                            <span>24,juillet 2022</span>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-map-marked-alt"></i>
+                            <span>Stade A.M.D</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+    </div>
+</div>
